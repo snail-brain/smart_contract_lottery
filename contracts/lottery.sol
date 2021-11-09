21593f6e849e7d0bfeb31d00afd5f1f3a7d0600b
snail-brain <snail-brain@protonmail.com>
2021-11-09 17:34:04 -0600

    finished lottery.sol

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -3,29 +3,85 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract lottery {
+contract lottery is VRFConsumerBase {
     address payable[] public players;
     uint256 public usdEntryFee;
+    uint256 public randomness;
     AggregatorV3Interface internal priceFeed;
+    address owner;
+    address payable public recentWinner;
+    Lottery_State public lottery_state;
+    uint256 public fee;
+    bytes32 public keyhash;
 
-    constructor(address _priceFeedAddress) public {
-        usdEntryFee = 50 * 10**18;
-        priceFeed = AggregatorV3Interface(_priceFeedAddress);
+    enum Lottery_State {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
     }
 
-    function enter() public {
+    constructor(
+        address _priceFeedAddress,
+        address _vrfcoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrfcoordinator, _link) {
+        usdEntryFee = 50 * 10**18;
+        priceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = Lottery_State.CLOSED;
+        owner = msg.sender;
+        fee = _fee;
+        keyhash = _keyhash;
+    }
+
+    modifier _onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function enter() public payable {
+        require(lottery_state == Lottery_State.OPEN);
+        require(msg.value >= getEntranceFee(), "Poors Not Allowed!");
         players.push(msg.sender);
     }
 
     function getEntranceFee() public view returns (uint256) {
-        (, int256 price, , , ) = priceFeed.latestRoundData;
-        uint256 adjustedPrice = Uint256(price) * 10**18;
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10;
         uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
         return costToEnter;
     }
 
-    function startLottery() public {}
+    function startLottery() public _onlyOwner {
+        require(lottery_state == Lottery_State.CLOSED);
+        lottery_state = Lottery_State.OPEN;
+    }
 
-    function endLottery() public {}
+    function endLottery() public _onlyOwner {
+        lottery_state = Lottery_State.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyhash, fee);
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == Lottery_State.CALCULATING_WINNER,
+            "You aren't there yet"
+        );
+        require(_randomness > 0, "random-not-found");
+
+        uint256 indexOfWinner = _randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+
+        //Reset
+        players = new address payable[](0);
+        lottery_state = Lottery_State.CLOSED;
+        randomness = _randomness;
+    }
 }
